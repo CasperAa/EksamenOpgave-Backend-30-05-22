679f2616212b408cae5ec481551c597279f57e1b
CasperAa <caspercasper.aakviststeengaard@gmail.com>
2022-06-15 18:52:17 +0200

    Changed CrossOrigen to be applicable with new domain

--- src/main/java/com/example/tourdefrance/api/CyclistController.java
+++ src/main/java/com/example/tourdefrance/api/CyclistController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/cyclist")
 public class CyclistController {
 
